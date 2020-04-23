0dc8133ea2e91f54c4a21ad9e77f33b55d47f285
rafagars <rafagar96@gmail.com>
2020-04-23 00:07:33 -0400

    New branch

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8b9373cb34f0296e4330d2216356b8981fcc2f27021812aa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///micro-post.db'
+db = SQLAlchemy(app)
+
+from app import models
+
+from app import views
