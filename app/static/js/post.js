427588039d98e916ad525ef613248084cff17565
rafagar96@gmail.com <rafagar96@gmail.com>
2020-05-08 22:29:22 -0400

    Added spinner

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -4,5 +4,11 @@ $(document).ready(
     }),
         $(".unlike a").on('click', function () {
         $(this).toggleClass('far');;
+    }),
+
+    $(".spinner-border").hide(),
+
+    $(".submit").on('click', function () {
+        $(".spinner-border").show()
     })
 );
