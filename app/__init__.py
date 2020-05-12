29e5865d257fd874c23f35007d10b686a5818ddb
rafagars <rafagar96@gmail.com>
2020-05-12 04:57:25 -0400

    Flask-momment added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8b9373cb34f0296e4330d2216356b8981fcc2f27021812aa'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///micro-post.db'
 db = SQLAlchemy(app)
+moment = Moment(app)
 
 from app import models
 
