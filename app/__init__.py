6cf205759dcb04ebda7743e457dad2c4effaae1b
rafagars <rafagar96@gmail.com>
2020-05-13 16:58:21 -0400

    Hidden secret key

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8b9373cb34f0296e4330d2216356b8981fcc2f27021812aa'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///micro-post.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -13,3 +14,4 @@ moment = Moment(app)
 from app import models
 
 from app import views
+
