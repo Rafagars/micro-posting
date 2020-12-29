fbb53da8f09b785052cc2a50fc1832f034203f85
rafagars <rafagar96@gmail.com>
2020-12-29 11:54:27 -0400

    Removed TinyMCE's premium plugins

--- app/static/js/tinymce.js
+++ app/static/js/tinymce.js
@@ -1,7 +1,7 @@
 tinymce.init({
   selector: '#mytextarea',
-  plugins: "a11ychecker casechange formatpainter linkchecker autolink lists checklist media mediaembed permanentpen powerpaste table advtable tinymcespellchecker emoticons image imagetools",
-  toolbar: "a11ycheck casechange checklist formatpainter permanentpen table emoticons image media",
+  plugins: "autolink lists media powerpaste table emoticons image imagetools",
+  toolbar: "table emoticons image media",
   toolbar_mode: 'floating',
   file_picker_types: 'image media',
   images_upload_url: '/imageuploader',
